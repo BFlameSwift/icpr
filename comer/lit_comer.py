003d99a4086cdc0002b71d3b51811b8c3c4aa997
BFlameSwift <bflameswift@163.com>
2024-06-12 18:00:27 +0800

    make validation later

--- comer/lit_comer.py
+++ comer/lit_comer.py
@@ -99,15 +99,15 @@ class LitCoMER(pl.LightningModule):
             sync_dist=True,
         )
         
-        # if self.current_epoch < self.hparams.milestones[0]:
-        #     self.log(
-        #         "val_ExpRate",
-        #         self.exprate_recorder,
-        #         prog_bar=True,
-        #         on_step=False,
-        #         on_epoch=True,
-        #     )
-        #     return
+        if self.current_epoch < self.hparams.milestones[0]-3:
+            self.log(
+                "val_ExpRate",
+                self.exprate_recorder,
+                prog_bar=True,
+                on_step=False,
+                on_epoch=True,
+            )
+            return
 
 
         hyps = self.approximate_joint_search(batch.imgs, batch.mask)
